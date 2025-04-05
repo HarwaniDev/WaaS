f87e297cd867ee0763ebb7cb6d94163c794dedcb
Dev Harwani <devharwani.work@gmail.com>
2025-04-06 03:26:12 +0530

    add get public key route

---
+++ src/app/api/getUserPubKey/route.ts
@@ -0,0 +1,45 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+async function getUserPublicKey(req: NextRequest) {
+    const session = await getServerSession();
+    if (!session) {
+        return NextResponse.json({ message: "Unauthorized" }, {status: 401})
+      }    
+
+    try {
+        const { name } = await req.json();
+        const user = await prisma?.user.findFirst({
+            where: {
+                name: name
+            }
+        });
+        if (!user) {
+            return NextResponse.json({
+                error: "No user with given name found"
+            }, {
+                status: 402
+            })
+        }
+        const wallet = await prisma?.solWallet.findFirst({
+            where: {
+                userId: user?.id
+            }
+        });
+        return NextResponse.json({
+            publicKey: wallet?.publicKey
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        return NextResponse.json({
+            error: "Something wrong at our side"
+        }, {
+            status: 500
+        })
+    }
+
+}
+
+export { getUserPublicKey as GET };
+
